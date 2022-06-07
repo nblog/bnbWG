d31039d09d6414d4778c47df178ceaef84d78128
r0th3r <503407184@qq.com>
2022-06-07 23:27:59 +0800

    更新

--- temp/bnbWG.js
+++ temp/bnbWG.js
@@ -22,53 +22,61 @@ function symbols() {
             "gaming": {
                 "aob" : {
                     "notes": "",
-                    "type": "call", 
+                    "mode": "call", 
                     "offset": ptr(0x35), 
                     "pattern": "6A 02 E8 ?? ?? ?? ?? 83 C4 04 39 45 F8 75 07 B8 01 00 00 00 EB 02 33 C0"
                 },
-                "rva": 0x1BF32ED,
+                "value": 0x1BF32ED,
             },
 
 
             "roles_object_call": {
                 "aob" : {
                     "notes": "",
-                    "type": "call", 
+                    "mode": "call", 
                     "offset": ptr(3), 
                     "pattern": "8B 4D F4 E8 ?? ?? ?? ?? 8B C8 E8 ?? ?? ?? ?? 89 45 F0 83 7D F0 00 75 02"
                 },
-                "rva": 0xAF0340,
+                "value": 0xAF0340,
             },
 
             "role_object_call": {
                 "aob" : {
                     "notes": "",
-                    "type": "rva", 
+                    "mode": "rva", 
                     "offset": -ptr(0x19), 
                     "pattern": "00 74 1D 8B 4D 08 51 E8"
                 },
-                "rva": 0x8BA150,
+                "value": 0x8BA150,
             },
 
             "positiont_call": {
                 "aob" : {
                     "notes": "",
-                    "type": "rva", 
+                    "mode": "rva", 
                     "offset": -ptr(0x41), 
                     "pattern": "8B 50 38 FF D2 85 C0 74 08 8B 4D FC E8"
                 },
-                "rva": 0x5FAB30,
+                "value": 0x5FAB30,
             },
 
             "car_call": {
                 "aob" : {
                     "notes": "",
-                    "type": "rva", 
+                    "mode": "rva", 
                     "offset": -ptr(0x3c), 
                     "pattern": "FF D0 89 45 F8 83 7D F8 FF 75 07 33 C0 E9"
                 },
-                "rva": 0x5FE2C0,
+                "value": 0x5FE2C0,
             },
+        },
+
+        "offsets": {
+
+            "role_opt_offset": {
+                "value": 0x584,
+            },
+
         }
 
     }
@@ -90,14 +98,27 @@ function relocator_addr(rva) {
 
 function get_bnb_main_object() {
     let roles_object = new NativeFunction(
-        relocator_addr( bnbWG.calls.roles_object_call.rva ), 
+        relocator_addr( bnbWG.calls.roles_object_call.value ), 
         "pointer", [], "thiscall");
 
     let role_object = new NativeFunction(
-        relocator_addr( bnbWG.calls.role_object_call.rva ), 
+        relocator_addr( bnbWG.calls.role_object_call.value ), 
         "pointer", ["pointer", "uint"], "thiscall");
 
     return role_object(roles_object(), 0);
+}
+
+
+function get_main_posXY() {
+    let bnb = get_bnb_main_object().add(bnbWG.offsets.role_opt_offset.value);
+    
+    let posXY = Memory.alloc(8);
+    let vtablefn = bnb.readPointer().add(25 * ptrlength).readPointer();
+    new NativeFunction(
+        vtablefn, "void", [ "pointer", "pointer" ], "thiscall"
+    )(bnb, posXY);
+
+    return { "X": posXY.add(0).readU32(), "Y": posXY.add(4).readU32() };
 }
 
 
@@ -105,7 +126,7 @@ function get_bnb_main_object() {
 
 function progress() {
 
-    let gaming = relocator_addr( bnbWG.calls.gaming.rva );
+    let gaming = relocator_addr( bnbWG.calls.gaming.value );
 
     Interceptor.attach(gaming.add(0x93), { 
         onEnter: function (args) {
@@ -113,6 +134,8 @@ function progress() {
             this.bnb_object = this.context.ebp.sub(ptrlength).readPointer();
             if ( 0 == get_bnb_main_object().compare(this.bnb_object) ) {
                 this.context.eax = ptr(0);
+
+                console.log( JSON.stringify(get_main_pos()) );
             }
         },
         onLeave: function (retval) {
@@ -144,16 +167,17 @@ rpc.exports = {
 
     pos() {
         new NativeFunction(
-            relocator_addr(bnbWG.calls.positiont_call.rva),
+            relocator_addr(bnbWG.calls.positiont_call.value),
             "void", ["pointer", "uint", "uint"] , "thiscall"
-        )(get_bnb_main_object(), 0, 36);
+        )(get_bnb_main_object(), 0, 38);
     },
 
     car(idx=7) {
+        let posXY = get_main_posXY();
         new NativeFunction(
-            relocator_addr(bnbWG.calls.car_call.rva),
+            relocator_addr(bnbWG.calls.car_call.value),
             "void", ["pointer", "uint", "uint", "uint"] , "thiscall"
-        )(get_bnb_main_object(), idx, 0, 36);
+        )(get_bnb_main_object(), idx, posXY.X * 40, (posXY.Y + 1) * 40);
     }
 }
 
